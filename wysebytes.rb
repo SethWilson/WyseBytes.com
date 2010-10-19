d17656b8cfe4f61464e2552bbbce015198cfff26
Seth Wilson <sethdubya@gmail.com>
2010-10-18 21:58:10 -0400

    Changin up main sinatra app for tighter oauth

--- wysebytes.rb
+++ wysebytes.rb
@@ -1,11 +1,12 @@
 require 'rubygems'
 require 'sinatra'
-require 'erb'
 require 'lib/gs'
 require 'lib/wiki_data'
 require 'oauth'
 require 'haml'
 
+
+enable :sessions
 
 configure do
   set :sessions, true
@@ -13,103 +14,49 @@ configure do
 end
 
 before do
-  next if request.path_info =~ /ping$/
-  @user = session[:user]
+  session[:oauth] ||= {}  
   
-  @con = OAuth::Consumer.new(ENV['CONSUMER_KEY'] || @@config['consumer_key'], ENV['CONSUMER_SECRET'] || @@config['consumer_secret'],
+  @consumer = OAuth::Consumer.new(ENV['CONSUMER_KEY'] || @@config['consumer_key'], ENV['CONSUMER_SECRET'] || @@config['consumer_secret'],
                                 {:site => 'https://www.google.com',
                                  :request_token_path => '/accounts/OAuthGetRequestToken',
                                  :access_token_path => '/accounts/OAuthGetAccessToken',
                                  :authorize_path => '/accounts/OAuthAuthorizeToken'})
   
-  
-end
-
-
-# store the request tokens and send to Google
-get '/connect' do
-  rt = @con.get_request_token({}, {:scope => ENV['SCOPE'] || @@config['SCOPE']})
-  
-  request_token = @client.request_token(
-    :oauth_callback => ENV['CALLBACK_URL'] || @@config['callback_url']
-  )
-  session[:request_token] = rt.token
-  session[:request_token_secret] = rt.secret
-  redirect rt.authorize_url
-end
-
-# auth URL is called by google after the user has accepted the application
-# this is configured on the Google manage my domain page
-get '/auth' do
-  # Exchange the request token for an access token.
-  
-  begin
-    @access_token = @client.authorize(
-      session[:request_token],
-      session[:request_token_secret],
-      :oauth_verifier => params[:oauth_verifier]
-    )
-  rescue OAuth::Unauthorized
+  if !session[:oauth][:request_token].nil? && !session[:oauth][:request_token_secret].nil?
+    @request_token = OAuth::RequestToken.new(@consumer, session[:oauth][:request_token], session[:oauth][:request_token_secret])
   end
   
-  if @client.authorized?
-      # Storing the access tokens so we don't have to go back to Twitter again
-      # in this session.  In a larger app you would probably persist these details somewhere.
-      session[:access_token] = @access_token.token
-      session[:secret_token] = @access_token.secret
-      session[:user] = true
-      redirect '/timeline'
-    else
-      redirect '/'
+  if !session[:oauth][:access_token].nil? && !session[:oauth][:access_token_secret].nil?
+    @access_token = OAuth::AccessToken.new(@consumer, session[:oauth][:access_token], session[:oauth][:access_token_secret])
+  end
+  
+  
+end
+
+get "/" do
+  if @access_token
+    
+    haml :index
+  else
+    '<a href="/request">Sign On</a>'
   end
 end
 
-
-get '/' do
-  if request.xhr?
-    "Hello, AJAX!"
-  else 
-   erb :index
- end
+get "/request" do
+  @request_token = @consumer.get_request_token(:oauth_callback => "http://#{request.host}/auth")
+  session[:oauth][:request_token] = @request_token.token
+  session[:oauth][:request_token_secret] = @request_token.secret
+  redirect @request_token.authorize_url
 end
 
-
-
-post '/sendjson' do
-
-  request.body.read
-
+get "/auth" do
+  @access_token = @request_token.get_access_token :oauth_verifier => params[:oauth_verifier]
+  session[:oauth][:access_token] = @access_token.token
+  session[:oauth][:access_token_secret] = @access_token.secret
+  redirect "/"
 end
 
-get '/:name' do
-    redirect '/', 307
-end
-
-post '/search/' do
-  "Got your post'd data #{params["search"]}"
-  
-  @email = "sethdubya@gmail.com"
-  @password = "1234Idtw4r!"
-  gs = SpreadsheetExamples.new()
-
-  gs.authenticate(@email, @password)
-
-  search_terms = [params["search"]]
-  
-  wiki_data = WikiData.new("Daily%20Wiki", search_terms, @email, @password)
-  
-  @wiki_entries = wiki_data.get_wiki_data()
-  
-  haml :search
-  
-end
-
-
-get '/disconnect' do
-  session[:user] = nil
-  session[:request_token] = nil
-  session[:request_token_secret] = nil
-  session[:access_token] = nil
-  session[:secret_token] = nil
-  redirect '/'
+get "/logout" do
+  session[:oauth] = {}
+  redirect "/"
 end
